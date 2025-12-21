0ac6b61ffb49a4c1bd71696045ce56803800fca4
PraBWa04 <vmatishchak04@gmail.com>
2025-12-21 23:33:09 +0100

    Add helper to capitalize first letter of word

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
+function capitalize(word) {
+  if (!word) return "";
+  return word[0].toUpperCase() + word.slice(1);
 }
 
-const numbers = [1, 2, 2, 3, 4, 4, 5];
-console.log(removeDuplicates(numbers));
+// function removeDuplicates(arr) {
+//   return [...new Set(arr)];
+// }
+
+// const numbers = [1, 2, 2, 3, 4, 4, 5];
+// console.log(removeDuplicates(numbers));
 
 // function toggleFlag(value) {
 //   return !value;
